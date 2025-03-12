ff987e5a1c35306b5b4d25c3fbdd43fec77234ee
dayoodukoya <dayoodukoya@gmail.com>
2025-03-12 16:54:55 +0100

    Delete css/js/script.js

--- css/js/script.js
+++
@@ -1,8 +0,0 @@
-function calculateScore() {
-    let score = 0;
-    let questions = document.querySelectorAll('input[type="radio"]:checked');
-    questions.forEach(function (question) {
-        score += parseInt(question.value);
-    });
-    document.getElementById('totalScore').textContent = 'Total Score: ' + score;
-}
